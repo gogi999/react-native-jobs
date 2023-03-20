3d27e54dcd7a060e22f6aa2827d6d59aa510d4e0
gogi999 <g.petrovic9@gmail.com>
2023-03-20 13:40:24 +0100

    Added basic setup and started Home page

---
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {
+  Text,
+  View,
+} from 'react-native';
+
+const NearbyJobCard = () => {
+  return (
+    <View>
+      <Text>NearbyJobCard</Text>
+    </View>
+  )
+}
+
+export default NearbyJobCard
